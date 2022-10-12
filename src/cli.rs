62dace0b0b848276dcd75e42da5d141510921386
Fernando Jascovich <fernando.ej@gmail.com>
2022-10-12 12:41:25 -0300

    cli logic separated

--- src/cli.rs
+++ src/cli.rs
@@ -62,7 +62,7 @@ pub enum Commands {
         path: Option<String>,
 
         #[command(subcommand)]
-        action: Action
+        kind: ActionKind
     },
 
     /// Similar to query but this will return a list of matched paths
@@ -88,74 +88,88 @@ pub enum Commands {
 }
 
 #[derive(Subcommand, Debug)]
-pub enum Action {
+pub enum ActionKind {
     Start,
     Stop
+}
+
+fn query(
+    tags: &Option<Vec<String>>,
+    path: &Option<String>,
+    start_date: &Option<NaiveDate>,
+    end_date: &Option<NaiveDate>
+) {
+    let results = if let Some(t) = tags {
+        if let Some(st) = start_date {
+            data::query::by_tags_and_date(t.clone(), &st, &end_date)
+        } else {
+            data::query::by_tags(t.clone())
+        }
+    } else if let Some(p) = path {
+        data::query::by_path(&p)
+    } else if let Some(p) = default_path() {
+        data::query::by_path(&p)
+    } else if let Some(st) = start_date {
+        data::query::by_date(&st, &end_date)
+    } else {
+        data::query::all()
+    };
+    table::print(results);
+}
+
+fn action(path: &Option<String>, kind: &ActionKind) {
+    let results = if let Some(p) = path  {
+        data::query::by_path(p)
+    } else {
+        data::query::by_path(&default_path().unwrap())
+    };
+    let mut doc = results.first().unwrap().clone();
+    match kind {
+        ActionKind::Start => {
+            doc.start();
+            data::update_entry(doc);
+        }
+        ActionKind::Stop => {
+            doc.stop();
+            data::update_entry(doc);
+        }
+    };
+}
+
+fn browse(active: &bool) {
+    let mut docs: Vec<data::model::DiaryDoc> = data::query::all();
+    if *active {
+        docs.retain(|x| x.is_active());
+    }
+    let paths: Vec<String> = docs
+        .iter()
+        .map(|x| String::from(&x.path))
+        .collect();
+    for x in paths {
+        println!("{}", x);
+    }
+}
+
+fn fetch(key: &str, path: &Option<String>) {
+    let ticket = jira::fetch(key).unwrap();
+    let p = if let Some(path_str) = path {
+        Some(path_str.as_str())
+    } else {
+        None
+    }; 
+    data::create_entry(ticket, p);
+    table::print(data::query::by_path(key));
 }
 
 pub fn main() {
     let cli = Args::parse();
     match &cli.command {
         Commands::Query { tags, path, start_date, end_date } => {
-            let results = if let Some(t) = tags {
-                if let Some(st) = start_date {
-                    data::query::by_tags_and_date(t.clone(), st, end_date)
-                } else {
-                    data::query::by_tags(t.clone())
-                }
-            } else if let Some(p) = path {
-                data::query::by_path(p)
-            } else if let Some(p) = default_path() {
-                data::query::by_path(&p)
-            } else if let Some(st) = start_date {
-                data::query::by_date(st, end_date)
-            } else {
-                data::query::all()
-            };
-            table::print(results);
+            query(tags, path, start_date, end_date);
         }
-        Commands::Action { path, action } => {
-            let results = if let Some(p) = path  {
-                data::query::by_path(p)
-            } else {
-                data::query::by_path(&default_path().unwrap())
-            };
-            let mut doc = results.first().unwrap().clone();
-            match action {
-                Action::Start => {
-                    doc.start();
-                    data::update_entry(doc);
-                }
-                Action::Stop => {
-                    doc.stop();
-                    data::update_entry(doc);
-                }
-            };
-
-        }
-        Commands::Browse { active } => {
-            let mut docs: Vec<data::model::DiaryDoc> = data::query::all();
-            if *active {
-                docs.retain(|x| x.is_active());
-            }
-            let paths: Vec<String> = docs
-                .iter()
-                .map(|x| String::from(&x.path))
-                .collect();
-            for x in paths {
-                println!("{}", x);
-            }
-        }
-        Commands::Fetch { key, path } => {
-            let ticket = jira::fetch(key).unwrap();
-            let p = if let Some(path_str) = path {
-                Some(path_str.as_str())
-            } else {
-                None
-            }; 
-            data::create_entry(ticket, p);
-            table::print(data::query::by_path(key));
-        }
+        Commands::Action { path, kind } => action(path, kind),
+        Commands::Browse { active } => browse(active),
+        Commands::Fetch { key, path } => fetch(key, path),
         Commands::SyncWorklog { } => todo!()
     }
 }
