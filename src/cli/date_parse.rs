20c90ef7aedc8a3f762448a0064e9388f7929ae2
Fernando Jascovich <fernando.ej@gmail.com>
2022-10-21 17:31:50 -0300

    Biweekly date query

--- src/cli/date_parse.rs
+++ src/cli/date_parse.rs
@@ -9,6 +9,10 @@ fn days_from_today(days: i64) -> NaiveDate {
     today().checked_sub_signed(Duration::days(days)).unwrap()
 }
 
+fn current_day() -> i64 {
+    today().day().into()
+}
+
 pub fn input(s: &str) -> Result<NaiveDate, String> {
     if let Ok(date) = NaiveDate::parse_from_str(s, "%Y-%m-%d") {
         return Ok(date)
@@ -16,10 +20,16 @@ pub fn input(s: &str) -> Result<NaiveDate, String> {
     match s {
         "today" => Ok(today()),
         "yesterday" => Ok(days_from_today(1)),
-        "month" => {
-            let current_day: i64 = today().day().into();
-            Ok(days_from_today(current_day - 1))
-        },
+        "month" => Ok(days_from_today(current_day() - 1)),
+        "biweekly" => {
+            let today = current_day();
+            let target = if today >= 15 { 
+                15
+            } else {
+                1
+            };
+            Ok(days_from_today(today - target))
+        }
         _  => Err(format!("Can't format input as date: {}", s))
     }
 }
